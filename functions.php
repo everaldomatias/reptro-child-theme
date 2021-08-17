07c24be32214027357200673409ebf738c00603f
everaldomatias <matiaseveraldo@gmail.com>
2021-08-17 19:55:32 -0300

    Initial version of the child theme

---
+++ functions.php
@@ -0,0 +1,26 @@
+<?php
+/*This file is part of Reptro Child Theme, reptro child theme.
+
+All functions of this file will be loaded before of parent theme functions.
+Learn more at https://codex.wordpress.org/Child_Themes.
+
+Note: this function loads the parent stylesheet before, then child theme stylesheet
+(leave it in place unless you know what you are doing).
+*/
+
+if ( ! function_exists( 'rc_enqueue_child_styles' ) ) {
+	function rc_enqueue_child_styles() {
+
+	    // loading parent style
+	    wp_register_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+	    wp_enqueue_style( 'parent-style' );
+
+	    // loading child style
+	    wp_register_style( 'reptro-child-style', get_stylesheet_directory_uri() . '/style.css' );
+	    wp_enqueue_style( 'reptro-child-style' );
+
+	}
+    add_action( 'wp_enqueue_scripts', 'rc_enqueue_child_styles' );
+}
+
+/* Write here your own functions */ 
